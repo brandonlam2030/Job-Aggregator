d8a444653fba744f6d1a6cb234f92ee8690954bd
Brandon Lam <brandonlam2030@gmail.com>
2026-02-22 19:11:28 -0800

    Cleaned code

--- App/duckDBExtract.py
+++ App/duckDBExtract.py
@@ -10,10 +10,7 @@ con = duckdb.connect("jobs.duckdb")
 con.execute("INSTALL postgres_scanner;")
 con.execute("LOAD postgres_scanner;")
 con.execute(f"ATTACH '{os.getenv("RAW_CONNECTION_URL")}' AS JA (TYPE POSTGRES);")
-# con.execute("CREATE TABLE jobs AS SELECT * FROM JA.Jobs;")
-#  con.execute("CREATE TABLE resumes AS SELECT * FROM JA.Resume")
 con.execute("CREATE OR REPLACE TABLE jobs AS SELECT * FROM JA.Jobs;")
 con.execute("CREATE OR REPLACE TABLE resumes AS SELECT * FROM JA.Resume;")
-
-print(con.execute("SELECT * FROM resumes LIMIT 10").df())
+print(con.execute("SHOW TABLES;").fetchall())
 
