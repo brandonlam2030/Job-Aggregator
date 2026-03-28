3c77a846f97b3d410a3623b358bffac7ac70f744
Brandon Lam <brandonlam2030@gmail.com>
2026-03-27 17:52:21 -0700

    Updated import

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, FastAPI
 import requests,time
-from . import models
+import app.models as models
 from .database import engine, SessionLocal
 from sqlalchemy import select
 from pydantic import BaseModel
